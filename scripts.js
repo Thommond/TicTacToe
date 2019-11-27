d527c0295f02d56888b4ef010c30d6062ad9549b
Thom Mondeaux <Thommond554@gmail.com>
2019-11-26 19:06:10 -0500

    I completed three main taskes 1. I cleaned up code with comments and removed stray debugging 2. I added a reset button to the code 3. I deleted stray files and I also moved the JavaScript to a script file instead of in the html.

---
+++ scripts.js
@@ -0,0 +1,133 @@
+//creating a table grid
+function columns() {
+  for(let columns = 0; columns < 3; columns++) {
+   let row = document.querySelector(".table").insertRow(columns);
+   let cell1 = row.insertCell(0);
+   let cell2 = row.insertCell(-1);
+   let cell3 = row.insertCell(-1);
+  }
+}
+function checkX(x)  {
+  return x == "X";
+}
+function checkO(o)  {
+  return o == "O" ;
+}
+//calling the function
+columns();
+//count is for player rotations
+let count =  0;
+//creating the array's for the winning feature
+let vertical1 = [];
+let vertical2 = [];
+let vertical3 = [];
+let horizontal1 = [];
+let horizontal2 = [];
+//loads the code inside the function while the page loads
+window.onload = function () {
+  //runs a function onclick of the table
+  document.querySelector('.table').onclick = function(ev) {
+    //count is equal to count plus one
+    count = count + 1;
+    //row index is pointing to the parent index
+    let rowIndex = ev.target.parentElement.rowIndex;
+    //cell is also pointing to the parent index
+    let cellIndex = ev.target.cellIndex;
+    //position is equal to query selector the table plus both index's
+    let position = document.querySelector('.table').rows[rowIndex].cells[cellIndex];
+    //a conditional for odd numbers
+    if(count == 1 || count == 3 || count == 5 || count == 7 || count == 9) {
+      //let text equal to the new text node of x
+    let text = document.createTextNode("X");
+    //an alert for the location of the cellindex
+    alert('Row = ' + rowIndex + ', Column = ' + cellIndex);
+    //adding text to the element
+    position.appendChild(text);
+    //disables the double click on table elments
+    position.style.pointerEvents = "none";
+    //a conditional for even numbers
+  }  else if (count == 2 || count == 4 || count == 6 || count == 8) {
+    //text is equal to the new string node of o
+    let text = document.createTextNode("O");
+    //alerting the position of the character as before
+    alert('Row = ' + rowIndex + ', Column = ' + cellIndex);
+    //adding the text to the element
+    position.appendChild(text);
+    //disables the double click as before
+    position.style.pointerEvents = "none";
+  }
+
+
+/* the below conditonals push the specified
+characters to the array */
+
+  if(rowIndex == 0 && cellIndex == 0) {
+  vertical1.push(position.innerHTML);
+  horizontal1.push(position.innerHTML);
+} else if(rowIndex == 1 && cellIndex == 1) {
+  vertical2.push(position.innerHTML);
+  horizontal1.push(position.innerHTML);
+  horizontal2.push(position.innerHTML);
+} else if(rowIndex == 2 && cellIndex == 2) {
+  vertical3.push(position.innerHTML);
+  horizontal1.push(position.innerHTML);
+} else if(rowIndex == 0 && cellIndex == 2) {
+  vertical1.push(position.innerHTML);
+  horizontal2.push(position.innerHTML);
+} else if(rowIndex == 2 && cellIndex == 0) {
+  vertical3.push(position.innerHTML);
+  horizontal2.push(position.innerHTML);
+} else if(rowIndex == 0) {
+  vertical1.push(position.innerHTML);
+} else if(rowIndex == 1) {
+  vertical2.push(position.innerHTML);
+} else if(rowIndex == 2) {
+  vertical3.push(position.innerHTML);
+}
+
+/* conditional's below check if the array's
+are 3 in length and  depending which is true then */
+if(vertical1.length == 3) {
+  if(vertical1.every(checkX) == true) {
+    alert("Player one wins!");
+  } else  if (vertical1.every(checkO) == true) {
+    alert("Player two wins!");
+  }
+}
+if(vertical2.length == 3) {
+  if(vertical2.every(checkX) == true) {
+    alert("Player one wins!");
+  } else  if (vertical2.every(checkO) == true) {
+    alert("Player two wins!");
+  }
+}
+if(vertical3.length == 3) {
+  if(vertical3.every(checkX) == true) {
+    alert("Player one wins!");
+  } else  if (vertical3.every(checkO) == true) {
+    alert("Player two wins!");
+  }
+}
+if(horizontal1.length == 3) {
+  if(horizontal1.every(checkX) == true) {
+    alert("Player one wins!");
+  } else  if (horizontal1.every(checkO) == true) {
+    alert("Player two wins!");
+  }
+}
+if(horizontal2.length == 3) {
+  if(horizontal2.every(checkX) == true) {
+    alert("Player one wins!");
+  } else  if (horizontal2.every(checkO) == true) {
+    alert("Player two wins!");
+  }
+}
+  }
+}
+
+//This button reloads the page
+let reload = document.querySelector('button');
+
+reload.onclick = function() {
+  location.reload(false);
+}
